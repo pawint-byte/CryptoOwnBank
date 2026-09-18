21084e01198ae78e627ddd5b712d10d8381cc248
Peter Wint <pawint@me.com>
2026-09-18 19:30:09 -0400

    Honesty: en.ts assembler stub (Scout restore in progress)

--- client/src/i18n/en.ts
+++ client/src/i18n/en.ts
@@ -1,1 +1,5 @@
-PLACEHOLDER
+import { p1 } from "./en.part1";
+import { p2 } from "./en.part2";
+import { p3 } from "./en.part3";
+
+export const en = new Function("return (" + p1 + p2 + p3 + ")")() as any;
